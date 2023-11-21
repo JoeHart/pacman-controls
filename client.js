23ee073af96c4db14cfb23a9b600fc878fb573cd
Joe Hart <joe@joehart.dev>
2023-11-21 05:05:24 +0000

    Initial commit

---
+++ client.js
@@ -0,0 +1,142 @@
+const io = require("socket.io-client");
+var Filter = require('bad-words'),
+  filter = new Filter();
+
+const host = process.env.HOST || "play.joehart.fun";
+const port = process.env.PORT || 3000;
+const address = `wss://${host}:${port}`;
+
+function hslToRgb(h, s, l) {
+  let r, g, b;
+  if (s == 0) {
+    r = g = b = l;
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    }
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function getRandomColor() {
+  // Generate a random hue from 0 to 1
+  const hue = Math.random();
+  // Convert the HSL color to RGB
+  const [r, g, b] = hslToRgb(hue, 1, 0.5);
+  // Return the RGB color in hexadecimal format
+  return '#' + [r, g, b].map(x => {
+    const hex = x.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  }).join('');
+}
+
+if (localStorage.getItem("color") == null) {
+  localStorage.setItem("color", getRandomColor());
+}
+
+const color = localStorage.getItem("color");
+const nameInput = document.getElementById("name");
+if (localStorage.getItem("playerId") == null) {
+  let uuid = self.crypto.randomUUID();
+  localStorage.setItem("playerId", uuid);
+}
+
+const name = localStorage.getItem("name");
+const playerId = localStorage.getItem("playerId")
+if (name) {
+  nameInput.value = name;
+}
+
+try {
+  const ghostDiv = document.getElementById("ghost");
+
+  console.log("connecting to " + address);
+  const socket = io();
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    console.log("submitting form");
+    localStorage.setItem("name", e.target[0].value);
+    ghostDiv.style.backgroundColor = `${color}`;
+    socket.emit("spawn", { id: playerId, name: filter.clean(e.target[0].value), color: color });
+    return false;
+  };
+
+  window.handleSubmitForm = handleSubmitForm
+  const leftButton = document.getElementById("left");
+  const rightButton = document.getElementById("right");
+  const upButton = document.getElementById("up");
+  const downButton = document.getElementById("down");
+
+  const handleLeftButton = () => {
+    console.log("left button clicked");
+    socket.emit("left", { id: playerId });
+  };
+  const handleRightButton = () => {
+    socket.emit("right", { id: playerId });
+  };
+  const handleUpButton = () => {
+    socket.emit("up", { id: playerId });
+  };
+  const handleDownButton = () => {
+    socket.emit("down", { id: playerId });
+  };
+  document.addEventListener('keydown', function (event) {
+    switch (event.key) {
+      case "ArrowDown":
+        socket.emit("down", { id: playerId });
+        break;
+      case "ArrowUp":
+        socket.emit("up", { id: playerId });
+        break;
+      case "ArrowLeft":
+        socket.emit("left", { id: playerId });
+        break;
+      case "ArrowRight":
+        socket.emit("right", { id: playerId });
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+  });
+
+  socket.on("connect", () => {
+  });
+
+  socket.on("die", () => {
+    // alert("You died!");
+    document.body.innerHTML = "<h1>You died!</h1>";
+  });
+
+  socket.on("roomFull", () => {
+    alert("Room is full!");
+  });
+
+  socket.on("increment", num => {
+    console.log("increment from server");
+    console.log(display);
+    display.innerHTML = num;
+  });
+
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  leftButton.onclick = handleLeftButton;
+  rightButton.onclick = handleRightButton;
+  upButton.onclick = handleUpButton;
+  downButton.onclick = handleDownButton;
+
+} catch (e) {
+  console.log(e);
+}
---
+++ server.js
@@ -0,0 +1,98 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+const asset_dir_path = "dist/";
+
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  var filename = path.basename(req.url);
+  var extension = path.extname(filename);
+  if (extension === '.js')
+    console.log("The file " + filename + " was requested.");
+  next();
+});
+// app.use(express.static(__dirname + "/dist"));
+app.use(express.static(asset_dir_path, {
+  index: false,
+  setHeaders: (response, file_path, file_stats) => {
+    // This function is called when “serve-static” makes a response.
+    // Note that `file_path` is an absolute path.
+
+    // Logging work
+    const relative_path = path.join(asset_dir_path, path.relative(asset_dir_path, file_path));
+    console.info(`@${Date.now()}`, "GAVE\t\t", relative_path);
+  }
+}));
+
+app.get("/", function (req, res) {
+  console.log("sending client.html");
+
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+
+
+
+const server = app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
+
+var io = require("socket.io")(server, {
+  origins: '*:*',
+  transports: ['websocket', 'polling']
+});
+
+
+let number = 0;
+
+let unitySocket;
+
+const playerIdToSocketIdMap = {}
+
+// io.attach(server);
+// console.log("starting websockets on port 3001");
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function (data) {
+    console.log("user disconnected", socket.id);
+
+    io.to(unitySocket).emit("destroy", { id: socket.id });
+  });
+  socket.on("spawn", data => {
+    playerIdToSocketIdMap[data.id] = socket.id;
+    io.to(unitySocket).emit("spawn", data);
+  });
+  socket.on("left", function ({ id }) {
+    console.log("LEFT");
+    io.to(unitySocket).emit("left", { id });
+  });
+  socket.on("die", function ({ id }) {
+    console.log("DIE", id);
+    console.log(playerIdToSocketIdMap)
+    io.to(playerIdToSocketIdMap[id]).emit("die");
+  });
+
+  socket.on("roomFull", ({ id }) => {
+    console.log("ROOM FULL", id);
+    io.to(playerIdToSocketIdMap[id]).emit("roomFull");
+  })
+  socket.on("right", function ({ id }) {
+    console.log("RIGHT");
+    io.to(unitySocket).emit("right", { id });
+  });
+  socket.on("down", function ({ id }) {
+    console.log("DOWN");
+    io.to(unitySocket).emit("down", { id });
+  });
+  socket.on("up", function ({ id }) {
+    console.log("UP");
+    io.to(unitySocket).emit("up", { id });
+  });
+  socket.on("registerGame", function (data) {
+    unitySocket = data.id;
+  });
+  socket.on("spawn", data => {
+    console.log("SPAWN: ", data);
+  });
+});
